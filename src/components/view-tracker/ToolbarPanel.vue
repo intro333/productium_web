562e4e1c73ae741a59ecc7f6006248c8784c40d0
DDerzhaev <d.derzhayev@spotapp.ru>
2020-10-10 12:33:41 +0300

    toolbar panel.

--- src/components/view-tracker/ToolbarPanel.vue
+++ src/components/view-tracker/ToolbarPanel.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="toolbar-panel">
-
+    <div class="toolbar-panel-left">
+      <div class="tp-icon-box p-cursor-pointer">
+        <img src="@/assets/img/case-tracker/toolbar_panel/logo.svg"
+             class="tp-icon-img"
+             alt="">
+      </div>
+      <div class="tp-icon-box p-cursor-pointer">
+        <img src="@/assets/img/case-tracker/toolbar_panel/menu.svg"
+             class="tp-icon-img"
+             alt="">
+      </div>
+    </div>
+    <div class="toolbar-panel-right">
+      
+    </div>
   </div>
 </template>
 
