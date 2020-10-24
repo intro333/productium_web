8a467f0ab978119a425634186bf3f0e4d764969b
DDerzhaev <d.derzhayev@spotapp.ru>
2020-10-24 22:53:13 +0300

    workarea: drop-zone, drop-handlers.

--- src/components/view-tracker/WorkArea.vue
+++ src/components/view-tracker/WorkArea.vue
@@ -1,11 +1,77 @@
 <template>
   <div class="work-area">
-    <p>Work AREA</p>
+    <div v-if="!isSlideImage"
+         @dragover="dragover"
+         @dragleave="dragleave"
+         @drop="drop"
+         ref="droppedZoneRef"
+         class="wa-empty"
+         :class="{'wa-empty-is-dropped': fileIsDragOver}">
+      <div class="wa-empty-info-box">
+        <p class="wa-empty-info rgb-black-50">перетащите файл сюда, или <span class="wa-empty-info-link"
+        >выберите файл</span></p>
+        <p class="wa-empty-info-more rgb-black-50">jpg, jpeg, png, svg</p>
+        <input ref="inputFileRef"
+               accept="image/png,image/jpeg,image/jpg,image/svg"
+               @change="uploadImageToCanvasBg"
+               type="file"
+               class="wa-empty-info-input">
+      </div>
+    </div>
+    <div v-if="isSlideImage">
+      <p>CANVAS</p>
+      <p>{{filename}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "WorkArea"
+  name: "WorkArea",
+  data: () => ({
+    isSlideImage: false,
+    fileIsDragOver: false,
+    filename: ''
+  }),
+  mounted() {
+    setTimeout(() => {
+      const _ref = this.$refs['droppedZoneRef'];
+      _ref.addEventListener('drop', function(e) {
+        console.log(1, e);
+      });
+    }, 200)
+  },
+  methods: {
+    uploadImageToCanvasBg($event) {
+      const files = $event.target.files;
+      this.setFile(files);
+    },
+    dragover($event) {
+      $event.preventDefault();
+      this.fileIsDragOver = true;
+    },
+    dragleave($event) {
+      $event.preventDefault();
+      this.fileIsDragOver = false;
+    },
+    drop($event) {
+      $event.preventDefault();
+      this.fileIsDragOver = false;
+      // const inputRef = this.$refs['inputFileRef']
+      const files = $event.dataTransfer.files;
+      this.setFile(files);
+    },
+    setFile(files) {
+      if (files && files[0]) {
+        const file = files[0];
+        this.isSlideImage = true;
+        this.filename = file.name;
+      }
+    }
+  }
 }
 </script>
+<!--
+https://kovart.github.io/dashed-border-generator/
+https://stenvdb.be/articles/building-a-vuejs-drag-and-drop-file-component
+-->
