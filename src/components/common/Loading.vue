4186bd4e55338f7fb70d4e92267e15468329a7a6
Dmitry <intro333@ya.ru>
2021-01-15 01:25:58 +0300

    loader.

--- src/components/common/Loading.vue
+++ src/components/common/Loading.vue
@@ -1,13 +1,45 @@
 <template>
     <div class="p-modal">
       <div class="p-modal-background p-modal-loading"></div>
-      <img class="loading-icon"
-           src="@/assets/img/common/loading.gif" alt="">
+      <div class="c-loader-popup">
+        <div class="lb-item grow-loader"
+             :class="{'grow-loader-active': active === 1}"></div>
+        <div class="lb-item grow-loader"
+             :class="{'grow-loader-active': active === 2}"></div>
+        <div class="lb-item grow-loader"
+             :class="{'grow-loader-active': active === 3}"></div>
+      </div>
+<!--      <img class="loading-icon"-->
+<!--           src="@/assets/img/common/loading.gif" alt="">-->
     </div>
 </template>
 
 <script>
   export default {
     name: "Loading",
+    data: () => ({
+      active: 0,
+      timeoutMs: 0,
+      timeoutId: 0,
+    }),
+    created() {
+      this.twist(1);
+    },
+    beforeDestroy() {
+      clearTimeout(this.timeoutId);
+    },
+    methods: {
+      twist(num) {
+        this.timeoutId = setTimeout(() => {
+          this.timeoutMs = 250;
+          this.active = num;
+          if (num === 3) {
+            this.twist(1);
+          } else {
+            this.twist(++num);
+          }
+        }, this.timeoutMs);
+      }
+    }
   }
 </script>
